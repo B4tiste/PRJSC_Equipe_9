8b73a77f1b99bbb5e10223e13015892619d08fb9
MaximeBattu <m.battu74@gmail.com>
2022-12-07 10:28:39 +0100

    add repositories

---
+++ src/main/java/Repositories/IncidentRepository.java
@@ -0,0 +1,8 @@
+package Repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public abstract class IncidentRepository extends BaseRepository {
+
+}
---
+++ src/main/java/Repositories/UrgenceRepository.java
@@ -0,0 +1,10 @@
+package Repositories;
+
+import Models.Urgence;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public abstract class UrgenceRepository extends BaseRepository {
+
+
+}
