53c8f7c6ea8fc9e81e10ce08c3c422613308396a
MaximeBattu <m.battu74@gmail.com>
2022-12-14 17:42:51 +0100

    add points and nearest one on click

--- projet-scientifique/src/components/Map.vue
+++ projet-scientifique/src/components/Map.vue
@@ -7,6 +7,7 @@
 <script>
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import * as turf from "@turf/turf";
 
 export default {
   data() {
@@ -18,10 +19,53 @@ export default {
       scrollWheelZoom: true,
       loading: true,
       options: null,
+      bounds: [
+        // en bas à gauche
+        [45.73041155658168, 4.791144701943372],
+        // en haut à droite
+        [45.79843412950511, 4.948728755139855],
+      ],
     };
   },
   async mounted() {
     this.initMap();
+
+    const grid = turf.pointGrid(
+      [
+        45.73041155658168, 4.791144701943372, 45.79843412950511,
+        4.948728755139855,
+      ],
+      500,
+      {
+        units: "meters",
+      }
+    );
+    console.log(grid);
+    var icon = L.icon({
+      iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
+      iconSize: [8, 14],
+      iconAnchor: [0, 0],
+      popupAnchor: [0, 0],
+    });
+
+    const map = this.map;
+    const capteurs = []
+    grid.features.forEach(function (marker) {
+      capteurs.push(turf.point(marker.geometry.coordinates))
+      L.marker(marker.geometry.coordinates, { icon: icon }).addTo(map);
+    });
+
+    const capteurFeatureCollection = turf.featureCollection(capteurs)
+
+    this.map.on("click", function (e) {
+      var coord = e.latlng;
+      var lat = coord.lat;
+      var lng = coord.lng;
+      const turfPoint = turf.point([lat, lng])
+      console.log(turfPoint, capteurFeatureCollection)
+      var nearest = turf.nearestPoint(turfPoint, capteurFeatureCollection);
+      console.log(nearest)
+    });
   },
   methods: {
     initMap() {
@@ -33,10 +77,13 @@ export default {
       this.map = L.map("map", {
         preferCanvas: true,
         zoomControl: false,
-        minZoom: 13,
-        maxZoom: 18,
-        maxBounds: L.latLngBounds(L.latLng(45.73041155658168, 4.791144701943372), L.latLng(45.79843412950511, 4.948728755139855)),
-      }).setView([45.785603052322436, 4.8694003049000285], 12)
+        minZoom: 14,
+        maxZoom: 20,
+        maxBounds: L.latLngBounds(
+          L.latLng(this.bounds[0]),
+          L.latLng(this.bounds[1])
+        ),
+      }).setView([45.785603052322436, 4.8694003049000285], 14);
 
       const lightLayer = L.tileLayer(
         "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=" +
@@ -52,36 +99,9 @@ export default {
           visible: true,
         }
       );
-      const darkLayer = L.tileLayer(
-        "https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=" +
-          process.env.VUE_APP_MAPBOX_ACCESS_TOKEN,
-        {
-          attribution: credits,
-          maxZoom: 22,
-          maxNativeZoom: 19,
-          id: "mapbox/streets-v11",
-          tileSize: 512,
-          zoomOffset: -1,
-          edgeBufferTiles: 2,
-          visible: false,
-        }
-      );
-      // Check the current theme of the browser
-      if (
-        window.matchMedia &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches
-      ) {
-        darkLayer.addTo(this.map);
-      } else {
-        lightLayer.addTo(this.map);
-      }
 
-      L.control
-        .layers({
-          "💡 Light theme": lightLayer,
-          "🌑 Dark theme": darkLayer,
-        })
-        .addTo(this.map);
+      lightLayer.addTo(this.map);
+
       L.control
         .scale({
           imperial: false,
@@ -90,24 +110,6 @@ export default {
           position: "bottomleft",
         })
         .addTo(this.map);
-
-      // L.GridLayer.GridDebug = L.GridLayer.extend({
-      //   createTile: function (coords) {
-      //     const tile = document.createElement("div");
-      //     tile.style.outline = "1px solid green";
-      //     tile.style.fontWeight = "bold";
-      //     tile.style.fontSize = "14pt";
-      //     tile.style.zIndex = "100";
-      //     tile.innerHTML = [coords.z, coords.x, coords.y].join("/");
-      //     return tile;
-      //   },
-      // });
-
-      // L.gridLayer.gridDebug = function (opts) {
-      //   return new L.GridLayer.GridDebug(opts);
-      // };
-
-      // this.map.addLayer(L.gridLayer.gridDebug());
     },
   },
 };
