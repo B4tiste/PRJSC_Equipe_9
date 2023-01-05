f8adc97afd9db034b34f6e6f82318a8713b37f0a
tristanwrona <tristan.wrona@cpe.fr>
2023-01-05 15:34:48 +0100

    dashboard

--- dashboard/send-mqtt.py
+++ dashboard/send-mqtt.py
@@ -1,5 +1,7 @@
 import paho.mqtt.client as mqtt
 import random
+import time
+
 
 #point="capteur,numero=4 intensity=10"
 
@@ -7,21 +9,24 @@ MQTT_SERVER = "127.0.0.1"
 MQTT_PORT = 1883
 MQTT_TOPIC = "capteur"
 
-client = mqtt.Client() #Création client MQTT
+while(True):
 
-try :
-    client.connect(MQTT_SERVER,MQTT_PORT)
-except :
-    print("Connection to MQTT server failed")
-    exit()
+    client = mqtt.Client() #Création client MQTT
 
-
-for i in range(66) :
-    point = f"capteur,numero={i} intensity={random.randint(0, 9)}"
-    try:
-        client.publish(MQTT_TOPIC,point)
-    except:
-        print("Publishing Error")
+    try :
+        client.connect(MQTT_SERVER,MQTT_PORT)
+    except :
+        print("Connection to MQTT server failed")
         exit()
 
-client.disconnect()
+
+    for i in range(66) :
+        point = f"capteur,numero={i} intensity={random.randint(0, 9)},etat={random.randint(0, 1)}"
+        try:
+            client.publish(MQTT_TOPIC,point)
+        except:
+            print("Publishing Error")
+            exit()
+
+    client.disconnect()
+    time.sleep(60)
